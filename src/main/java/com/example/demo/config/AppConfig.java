da3dba9bc225e9b8d85ad28eb366d67c53a5de51
Ollayorbek <ollayorbek2005@gmail.com>
2025-03-20 00:00:45 +0500

    app config added

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app.config")
+public class AppConfig {
+    private String title;
+    private String version;
+    private String adminEmail;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+}
